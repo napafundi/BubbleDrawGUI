250b863e87b8ccebbcbbe7203ec4aced4d572f1b
Nick Pafundi <napafundi@gmail.com>
2019-08-19 18:31:43 -0400

    bubble draw app with GUI components

---
+++ src/BubbleDrawGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+public class BubbleDrawGUI extends JFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Nick's BubbleDraw GUI App");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new BubblePanel());
+        frame.setSize(new java.awt.Dimension(600, 400));
+        frame.setVisible(true);
+    }
+}
